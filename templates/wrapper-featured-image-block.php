917892327ddca7c2988bd948880772fd8225f107
Michelle Schulp <michelle@marktimemedia.com>
2020-09-29 09:04:48 -0500

    small fix for featured image class

--- templates/wrapper-featured-image-block.php
+++ templates/wrapper-featured-image-block.php
@@ -10,5 +10,5 @@ if ( ! empty( $block['align'] ) ) {
 }
 $url = has_post_thumbnail() ? get_the_post_thumbnail_url() : wp_get_attachment_image_src( get_theme_mod( 'mtm_default_image' ), 'medium' )[0]; ?>
 <section class="mtm-grid--image <?php echo esc_attr( $classes ); ?>">
-	<a aria-hidden="true" tabindex="-1" href="<?php the_permalink(); ?>"><figure class="post--thumbnail mtm-post-thumbnail has-background cropped" style="background-image:url(<?php echo esc_url( $url ); ?>)"></figure></a>
+	<a aria-hidden="true" tabindex="-1" href="<?php the_permalink(); ?>"><figure class="post--thumbnail mtm-post-thumbnail has-background-image cropped" style="background-image:url(<?php echo esc_url( $url ); ?>)"></figure></a>
 </section>
